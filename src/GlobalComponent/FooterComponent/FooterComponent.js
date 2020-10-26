63082a84025ec07891ddd2a1ed1cec2e647dd1e9
LAPTOP-NR3HN8TL\Riddhi <riddhi.patalia@gmail.com>
2020-10-26 09:15:00 +0530

    Footer component added

---
+++ src/GlobalComponent/FooterComponent/FooterComponent.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class FooterComponent extends Component{
+    render(){
+        return(
+            <div>
+                <div className ="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                        <p>Copyright © 2020 - ALL RIGHTS RESERVED</p>
+                        <p>All contents of www.enago.com are protected under the International Copyright Act. All rights on this Web Site are reserved and no part of this Site shall be reproduced, stored in a retrieval system, or transmitted in any form or by any means - electronic, electrostatic, magnetic tape, mechanical, printing, photocopying, recording or otherwise, including the right of translation in any language, without the permission of Crimson Interactive Inc. (USA)</p>
+                        </div>
+                    </div>
+                </div>
+              
+            </div>
+        )
+    }
+}
+
+export default FooterComponent;
--- src/HomeComponent/HomeComponent.js
+++ src/HomeComponent/HomeComponent.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import HeaderComponent from '../GlobalComponent/HeaderComponent/HeaderComponent';
+import FooterComponent from '../GlobalComponent/FooterComponent/FooterComponent';
 class HomeComponent extends Component{
     render(){
         return(
             <div>
                 <HeaderComponent />
+                <FooterComponent />
             </div>
         )
     }
