2561533c4b53421814584d7560c20d95923542ea
amit204 <amitchakra3@gmail.com>
2020-11-01 20:50:55 +0530

    owl carousel added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-
 import HeaderComponent from './GlobalComponent/HeaderComponent/HeaderComponent';
 import FooterComponent from './GlobalComponent/FooterComponent/FooterComponent';
 import NavComponent from './GlobalComponent/NavComponent/NavComponent'
+
+
+
 function App() {
   return (
     <div>
--- src/SliderComponent/SliderComponent.js
+++ src/SliderComponent/SliderComponent.js
@@ -1,55 +1,35 @@
 import React, {Component} from 'react';
 //Owl Carousel Libraries and Module
+import $ from "jquery";
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
-const options = {
-    margin: 30,
-    responsiveClass: true,
-    nav: false,
-    autoplay: true,
-    navText: ["Prev", "Next"],
-    smartSpeed: 1000,
-    responsive: {
-        0: {
-            items: 1,
-        },
-        400: {
-            items: 1,
-        },
-        600: {
-            items: 2,
-        },
-        700: {
-            items: 3,
-        },
-        1000: {
-            items: 5,
-        }
-    },
-  };
+
 class SliderComponent extends Component{
 render() {
     return (
         <div>
-     <OwlCarousel className="slider-items owl-carousel" {...options}>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                      <div class="item"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></div>
-                  </OwlCarousel>
+    <div class="owl-carousel owl-theme">
+  <div> Your Content </div>
+  <div> Your Content </div>
+  <div> Your Content </div>
+  <div> Your Content </div>
+  <div> Your Content </div>
+  <div> Your Content </div>
+  <div> Your Content </div>
+</div>
 
         </div>
+        
     );
 }
+
 }
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+  });
 export default SliderComponent;
