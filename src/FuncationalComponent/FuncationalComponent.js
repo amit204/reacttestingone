e6eb1f22dc84324a12c44ba84a2b79406b54d54b
LAPTOP-NR3HN8TL\Riddhi <riddhi.patalia@gmail.com>
2020-11-07 17:48:11 +0530

    required field validation

--- src/FuncationalComponent/FuncationalComponent.js
+++ src/FuncationalComponent/FuncationalComponent.js
@@ -1,22 +1,58 @@
-import React, { useState, useEffect } from 'react';
-
-function FuncationalComponent() {
-  const [count, setCount] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-   
-  });
-  document.title = `You clicked ${count} times`;
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import React, { Component } from 'react';
+import validator from 'validator';
+const required = (value) => {
+  if (!value.toString().trim().length) {
+    // We can return string or jsx as the 'error' prop for the validated Component
+    return 'require';
+  }
+};
+ 
+const email = (value) => {
+  if (!validator.isEmail(value)) {
+    return `${value} is not a valid email.`
+  }
+};
+ 
+const lt = (value, props) => {
+  // get the maxLength from component's props
+  if (!value.toString().trim().length > props.maxLength) {
+    // Return jsx
+    return <span className="error">The value exceeded {props.maxLength} symbols.</span>
+  }
+};
+ 
+const password = (value, props, components) => {
+  // NOTE: Tricky place. The 'value' argument is always current component's value.
+  // So in case we're 'changing' let's say 'password' component - we'll compare it's value with 'confirm' value.
+  // But if we're changing 'confirm' component - the condition will always be true
+  // If we need to always compare own values - replace 'value' with components.password[0].value and make some magic with error rendering.
+  if (value !== components['confirm'][0].value) { // components['password'][0].value !== components['confirm'][0].value
+    // 'confirm' - name of input
+    // components['confirm'] - array of same-name components because of checkboxes and radios
+    return <span className="error">Passwords are not equal.</span>
+  }
+};
+export default class FuncationalComponent extends Component {
+    render() {
+        return <Form>
+            <h3>Login</h3>
+            <div>
+                <label>
+                    Email*
+                    <Input  name='email' validations={[required, email]}/>
+                </label>
+            </div>
+            <div>
+                <label>
+                    Password*
+                    <Input type='password' name='password' validations={[required, password]}/>
+                </label>
+            </div>
+            <div>
+               <button>submit</button>
+            </div>
+        </Form>;
+    }
 }
-export default FuncationalComponent
